52ffb4f1b4bdffdf2933047692880c995e91ae67
vanshika15007 <vanshika02007S@gmail.com>
2025-04-27 23:24:49 +0530

    update index.html

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return (
     <div className="flex items-center justify-center">
       <img 
-        src="src\assets\images\Capturee.JPG" 
+        src="src\assets\images\Capture.JPG" 
         alt="DisasterAI Logo"
         className="w-11 h-11
         rounded-full"
